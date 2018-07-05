d6ac5ad5ab57fb74147cb1e14fc3e46c059e4d54
James Williams <jamwil@gmail.com>
2018-07-04 21:25:37 -0600

    Fixed duplicates (hopefully)

--- terra.py
+++ terra.py
@@ -232,7 +232,7 @@ class Spin:
         """
         if len(self.data) > 0:
             self.journal = self.data[0].append(self.data[1:])
-        self.journal = self.journal.drop_duplicates()
+        self.journal = self.journal[~self.journal.index.duplicated(keep='first')]
         self.journal = self.journal.sort_values(by=['Registration Date'], ascending=False)
         return self.journal
 
